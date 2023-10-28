8272ec5dbd1f5c384bef7ab254789a6e95b0f2fc
da-the-dev <leshiktk@me.com>
2023-10-28 17:54:12 +0300

    Commented out cout for debug conveniece

--- main.cpp
+++ main.cpp
@@ -5,26 +5,27 @@
 using namespace std;
 
 int main() {
-  cout << "Enter the number of variables and the number of constraints:"
-       << endl;
+  // cout << "Enter the number of variables and the number of constraints:"
+  //      << endl;
   int variables;   // number of variables
   int constraints; // number of constraints
   cin >> variables;
   cin >> constraints;
 
-  cout << "Enter the C matrix:" << endl;
-  Matrix C(1, variables);
+  // cout << "Enter the C matrix:" << endl;
+  Matrix C(constraints, variables);
   cin >> C;
 
-  cout << "Enter the A vector (objective function):" << endl;
-  Matrix A(constraints, variables);
+  // cout << "Enter the A vector (objective function):" << endl;
+  Matrix A(variables, 1);
   cin >> A;
 
-  cout << "Enter the b vector (right-hand side):" << endl;
+  // cout << "Enter the b vector (right-hand side):" << endl;
   Matrix b(constraints, 1);
   cin >> b;
 
-  cout << "Enter the approximation accuracy (as in std::setprecision):" << endl;
+  // cout << "Enter the approximation accuracy (as in std::setprecision):" <<
+  // endl;
   int approximation;
   cin >> approximation;
 
